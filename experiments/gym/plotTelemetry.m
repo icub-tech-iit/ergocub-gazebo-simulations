cea3c8c12857ce72ee580ba741931af89d4d4f33
GrmanRodriguez <grmanrod@gmail.com>
2022-01-25 16:54:17 +0100

    added front raise and lateral raise experiments, also increased maxTorque in stickbot

--- experiments/gym/plotTelemetry.m
+++ experiments/gym/plotTelemetry.m
@@ -2,17 +2,20 @@ clc
 clear
 
 %% Modify this array to change the joints that will be plotted
-joints_to_plot = ["r_shoulder_pitch" "r_elbow" "r_wrist_prosup"];
+joints_to_plot = ["r_shoulder_pitch" "r_shoulder_roll" "r_shoulder_yaw" "r_elbow"];
 
 %% Start time of the plot
-start_time = 22;
+start_time = 31;
 
 %% Modify these two variables affect the title in the plots
-box_weight = 5.0;
-increase_factor = 1.0;
+box_weight = 7;
+increase_factor = 1.16;
+
+%% Frequency of the low pass filter
+cutoff_freq = 1e4;
 
 %% Read mat files
-testfiledir = './telemetry_data';
+testfiledir = './telemetry_data_curls_116';
 matfiles = dir(fullfile(testfiledir, '*.mat'));
 nfiles = length(matfiles);
 data  = cell(nfiles);
@@ -26,7 +29,7 @@ if nfiles == 0
 end
 
 timestamp_encoders = [];
-encoders = [];
+encoders = []; 
 timestamp_velocity = [];
 velocity = [];
 timestamp_torque = [];
@@ -60,6 +63,10 @@ timestamp_encoders = timestamp_encoders - timestamp_encoders(1,1);
 timestamp_velocity = timestamp_velocity - timestamp_velocity(1,1);
 timestamp_torque   = timestamp_torque - timestamp_torque(1,1);
 
+sampling_freq_encoders = 1 / (timestamp_encoders(1,2) - timestamp_encoders(1,1));
+sampling_freq_velocity = 1 / (timestamp_velocity(1,2) - timestamp_velocity(1,1));
+sampling_freq_torque   = 1 / (timestamp_torque(1,2) - timestamp_torque(1,1));
+
 tmp = find(timestamp_encoders > start_time);
 start_index_encoders = tmp(1);
 
@@ -70,7 +77,21 @@ tmp = find(timestamp_torque > start_time);
 start_index_torque = tmp(1);
 
 %% Plot
-title_prefix = strcat("x",num2str(increase_factor)," stickBot, box ", num2str(box_weight), "kg");
+
+torques_to_plot = torque(indices_to_plot,start_index_encoders:end);
+[amount_of_torques_to_plot, ~] = size(torques_to_plot);
+for i = 1 : amount_of_torques_to_plot
+    torques_to_plot(i,:) = lowpass(torques_to_plot(i,:), sampling_freq_torque, cutoff_freq);
+end
+
+velocities_to_plot = velocity(indices_to_plot,start_index_encoders:end);
+[amount_of_velocities_to_plot, ~] = size(velocities_to_plot);
+for i = 1 : amount_of_torques_to_plot
+    velocities_to_plot(i,:) = lowpass(velocities_to_plot(i,:), sampling_freq_velocity, cutoff_freq);
+end
+
+
+title_prefix = strcat("x",num2str(increase_factor)," stickBot, dumbells ", num2str(box_weight), "kg");
 figure
 plot(timestamp_encoders(start_index_encoders:end), encoders(indices_to_plot,start_index_encoders:end), 'LineWidth', 2)
 title(strcat(title_prefix, " encoders"))
@@ -80,7 +101,7 @@ xlabel('Time [s]')
 ylabel('Encoders [deg]')
 
 figure
-plot(timestamp_velocity(start_index_velocity:end), velocity(indices_to_plot,start_index_velocity:end), 'LineWidth', 2)
+plot(timestamp_velocity(start_index_velocity:end), velocities_to_plot, 'LineWidth', 2)
 title(strcat(title_prefix, " velocity"))
 legend(joints_to_plot, 'Interpreter', 'None')
 xlim([timestamp_velocity(start_index_velocity) max(timestamp_velocity)])
@@ -88,7 +109,7 @@ xlabel('Time [s]')
 ylabel('Velocity [deg/s]')
 
 figure
-plot(timestamp_torque(start_index_torque:end), torque(indices_to_plot,start_index_torque:end), 'LineWidth', 2)
+plot(timestamp_torque(start_index_torque:end), torques_to_plot, 'LineWidth', 2)
 title(strcat(title_prefix, " torque"))
 legend(joints_to_plot, 'Interpreter', 'None')
 xlim([timestamp_torque(start_index_torque) max(timestamp_torque)])
