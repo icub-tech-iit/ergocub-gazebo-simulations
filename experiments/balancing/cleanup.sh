76c611b08c8dd36f04642f33504b5af3409a812e
GrmanRodriguez <grmanrod@gmail.com>
2022-02-17 17:30:22 +0100

    add balancing experiment

---
+++ experiments/balancing/cleanup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Cleaning up"
+sleep 1
+killall -9 yarpserver
+killall -9 ctpService
+killall -9 gazebo
+killall -9 gzserver
+killall -9 gzclient
+killall -9 WalkingModule
+killall -9 matlab
